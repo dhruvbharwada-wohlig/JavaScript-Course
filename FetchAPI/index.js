9b02dd6f69fcefb3b22bbd8ae4b0869b6ecd1f71
Dhruv <dhruv.bharwada@wohlig.com>
2026-05-25 12:21:26 +0530

    Added fetch API files

---
+++ FetchAPI/index.js
@@ -0,0 +1,10 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+
+
+
+const getFacts = async() =>{
+  let response = await fetch(URL);
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
+}
